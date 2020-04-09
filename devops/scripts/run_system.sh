e5ca60a72cf4a457130fe7b364011bd1dbd61b9e
hark130 <hark130@yahoo.com>
2020-04-09 16:05:46 -0500

    system: Big run_system.sh step forward
    Mostly(?) done with run_system.sh.
    This commit includes:
    - validate_input_not_empty()
    - find_in_file()
    - Change directory
    - Call the necessary Makefile recipe
    - Pre-Cleanup
    - Script execution
    - Result validation 2 of 3 (stdout, stderr, but no *_out.txt)
    - Error/Failure handling all along the way
    
    TO DO: DON'T DO NOW:
    1. Finish the result validation by implementing *_out.txt
    2. Consider post-cleanup (but probably don't... because 'forensics')
    3. Transition script execution to binary execution
    4. Transition binary from "echo params" to "execute params" by calling system()
    5. Document results
    6. Merge
    
    Changes to be committed:
    	modified:   devops/scripts/run_system.sh

--- devops/scripts/run_system.sh
+++ devops/scripts/run_system.sh
@@ -13,8 +13,78 @@
 SUCCESS_PREFIX="Success:"  # Use this when the test results are favorable
 FAILURE_PREFIX="FAILURE!"  # Use this when some aspect of the shell script errors
 
+
+# Purpose - Tests parameter 1 for length 0
+# Return - 1 if empty, 0 if not
+validate_input_not_empty()
+{
+    if [ ${#1} -gt 0 ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+# Purpose - Searches for a string-needle in a file-haystack
+# Parameters
+#   First parameter - "Needle" string
+#   Second parameter - "Haystack" filename
+# Notes
+#   - Utilizes grep
+# Return - 0 if found, 1 if not, 2 on error
+find_in_file()
+{
+    # LOCAL VARIABLES
+    PARAM_NEEDLE=$1
+    PARAM_HAYSTACK=$2
+    RETVAL=0
+
+    # INPUT VALIDATION
+    # echo $PARAM_HAYSTACK  # DEBUGGING
+    # PARAM_NEEDLE
+    validate_input_not_empty $PARAM_NEEDLE
+    if [ $? -ne 0 ]
+    then
+        echo -e "\n"$FAILURE_PREFIX "The 'needle' parameter is invalid\n" >&2
+        return 2
+    fi
+    # PARAM_HAYSTACK
+    validate_input_not_empty $PARAM_HAYSTACK
+    if [ $? -ne 0 ]
+    then
+        echo -e "\n"$FAILURE_PREFIX "The 'haystack' parameter is invalid\n" >&2
+        return 2
+    fi
+    # Verify haystack exists
+    test -f $PARAM_HAYSTACK
+    if [ $? -ne 0 ]
+    then
+        echo -e "\n"$FAILURE_PREFIX "The 'haystack' "$PARAM_HAYSTACK" does not exist\n" >&2
+        return 2
+    fi
+
+    # SEARCH
+    grep "$PARAM_NEEDLE" $PARAM_HAYSTACK > /dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
+        return 0  # Found it
+    else
+        echo -e "\n"$FAILURE_PREFIX "Unable to find" $PARAM_NEEDLE "in" $PARAM_HAYSTACK"\n" >&2
+        return 1  # Didn't find it
+    fi
+
+    # DONE(?!)
+    return 2  # If you made it here, something went wrong
+}
+
+
+TEST_NUM=1  # Always start with the first test
+PYTHON_COMMAND="python3"
 SOURCE_DIR="src/python/"
 DIST_DIR="dist/"
+LOG_DIR="devops/logs/"
 PYTHON_FILE_1="python_script01.py"
 PYTHON_FILE_2="python_script02.py"
 PYTHON_FILE_3="python_script03.py"
@@ -27,6 +97,14 @@ PYTHON_REL_FILE_3=$SOURCE_DIR$PYTHON_FILE_3
 PYTHON_REL_FILE_4=$SOURCE_DIR$PYTHON_FILE_4
 PYTHON_REL_FILE_5=$SOURCE_DIR$PYTHON_FILE_5
 PYTHON_REL_FILE_6=$DIST_DIR$PYTHON_FILE_6
+EXAMPLE_ARG="-c"
+INPUT_FILE_PREFIX="python_script0"
+INPUT_FILE_SUFFIX="_in.txt"
+INPUT_FILE_PATH=$SOURCE_DIR"test_files/"
+TEMP_LOG_FILE_STDOUT="temp_log_stdout.txt"
+TEMP_LOG_FILE_STDERR="temp_log_stderr.txt"
+TEMP_LOG_REL_FILE_STDOUT=$LOG_DIR$TEMP_LOG_FILE_STDOUT
+TEMP_LOG_REL_FILE_STDERR=$LOG_DIR$TEMP_LOG_FILE_STDERR
 
 # CHANGE DIRECTORY
 cd "$(dirname "$0")"  # Change directory with respect to this script
@@ -40,16 +118,72 @@ then
     echo "Replicate these results with the following command:" >&2
     echo -e "make system\n" >&2
     exit 1
-fi 
+fi
 
-# EXECUTE
-for INPUT in $PYTHON_REL_FILE_1 $PYTHON_REL_FILE_2 $PYTHON_REL_FILE_3 $PYTHON_REL_FILE_4 $PYTHON_REL_FILE_5 $PYTHON_REL_FILE_6
+# PRE-CLEANUP
+for TEMP_FILE in $TEMP_LOG_REL_FILE_STDOUT $TEMP_LOG_REL_FILE_STDERR
 do
-    test -f $INPUT
+    test -f $TEMP_FILE
     if [ $? -eq 0 ]
     then
-        echo $SUCCESS_PREFIX "Found" $INPUT
-    else
-        echo $FAILURE_PREFIX "Unable to find" $INPUT
+        rm -f $TEMP_FILE
+        if [ $? -ne 0 ]
+        then
+            echo -e "\n"$FAILURE_PREFIX" failed to delete "$TEMP_FILE"\n" >&2
+        fi
     fi
 done
+
+# EXECUTE
+for PYTHON_FILE in $PYTHON_REL_FILE_1 $PYTHON_REL_FILE_2 $PYTHON_REL_FILE_3 $PYTHON_REL_FILE_4 $PYTHON_REL_FILE_5 $PYTHON_REL_FILE_6
+do
+    # LOOP VARIABLES
+    TEMP_COMMAND=$PYTHON_COMMAND" "$PYTHON_FILE" "$EXAMPLE_ARG" "$INPUT_FILE_PATH$INPUT_FILE_PREFIX$TEST_NUM$INPUT_FILE_SUFFIX
+    TEMP_TEST_NAME=$INPUT_FILE_PREFIX$TEST_NUM".py"
+    TEMP_STDOUT_NEEDLE="This is "$TEMP_TEST_NAME" stdout"
+    TEMP_STDERR_NEEDLE="This is "$TEMP_TEST_NAME" stderr"
+    TEMP_NUM_ERRORS=0
+
+    # Execute the command
+    $TEMP_COMMAND 1> $TEMP_LOG_REL_FILE_STDOUT 2> $TEMP_LOG_REL_FILE_STDERR
+    if [ $? -eq 0 ]
+    then
+        # echo $SUCCESS_PREFIX "Found" $INPUT
+        # echo $INPUT_FILE_PATH$INPUT_FILE_PREFIX$TEST_NUM$INPUT_FILE_SUFFIX
+        # echo $TEMP_COMMAND
+        # echo $SUCCESS_PREFIX "for" $PYTHON_FILE
+        # cat $TEMP_LOG_REL_FILE_STDOUT
+        # cat $TEMP_LOG_REL_FILE_STDERR
+
+        # VALIDATION
+        # 1. stdout
+        find_in_file "$TEMP_STDOUT_NEEDLE" $TEMP_LOG_REL_FILE_STDOUT
+        if [ $? -ne 0 ]
+        then
+            ((TEMP_NUM_ERRORS=TEMP_NUM_ERRORS+1))
+        fi
+        # 2. stderr
+        find_in_file "$TEMP_STDERR_NEEDLE" $TEMP_LOG_REL_FILE_STDERR
+        if [ $? -ne 0 ]
+        then
+            ((TEMP_NUM_ERRORS=TEMP_NUM_ERRORS+1))
+        fi
+        # 3. *_out.txt
+        # find_in_file
+        # 4. Summary
+        if [ $TEMP_NUM_ERRORS -eq 0 ]
+        then
+            echo "[X]" $TEMP_TEST_NAME "passed"
+        else
+            echo "[ ]" $TEMP_TEST_NAME "FAILED!"
+        fi
+    else
+        echo -e "\n"$FAILURE_PREFIX "Python failed" >&2
+        echo "Replicate the error with the following command:" >&2
+        echo -e $TEMP_COMMAND"\n" >&2
+    fi
+    ((TEST_NUM=TEST_NUM+1))
+done
+
+# DONE
+echo ""
