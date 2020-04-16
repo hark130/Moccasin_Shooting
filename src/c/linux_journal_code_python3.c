0bd1752ecc60c5b9a08ddfc8fc7d10c91409659e
hark130 <hark130@yahoo.com>
2020-04-16 17:49:42 -0500

    python_linking: Done monkeying with l_j_c_p3.c
    This version compiles and works.  I'm leaving it as it to transition to
    production(-ish) code.
    
    Changes to be committed:
    	modified:   src/c/linux_journal_code_python3.c
    Untracked files:
    	src/c/main_libpython.c

--- src/c/linux_journal_code_python3.c
+++ src/c/linux_journal_code_python3.c
@@ -29,20 +29,87 @@ void process_file(char *filename)
     }
 }
 
+// Lifted from:
+// https://stackoverflow.com/questions/31271158/cannot-convert-char-to-wchar-t
+wchar_t **convert_argv_char_to_wchar(int argc, char **argv)
+{
+    wchar_t **changed_argv = NULL;
 
-int main(int argc, char** argv)
+    changed_argv = calloc(argc + 1, sizeof(wchar_t *));
+
+    fprintf(stderr, "Allocated wchart_t **: %p\n", changed_argv);  // DEBUGGING
+    for (int i = 0; i < argc; i++)
+    {
+        changed_argv[i] = calloc(strlen(argv[i]) + 1, sizeof(wchar_t));
+        fprintf(stderr, "Copying %p into %p\n", argv[i], changed_argv[i]);  // DEBUGGING
+        mbstowcs(changed_argv[i], argv[i], strlen(argv[i]) + 1);
+    }
+
+    return changed_argv;
+}
+
+
+void process_file_wargs(char *filename, int argc, wchar_t **argv)
+{
+    // FILE *python_file = NULL;
+
+    // python_file = fopen(filename, "r");
+
+    // if (python_file)
+    {
+        fprintf(stderr, "Executing Py_Main()\n");  // DEBUGGING
+        Py_Main(argc, argv);
+        // fprintf(stderr, "Executing PyRun_SimpleFile()\n");  // DEBUGGING
+        // PyRun_SimpleFile(python_file, filename);
+        // fclose(python_file);
+        // python_file = NULL;
+    }
+}
+
+
+// Process a file along with arguments
+// void process_file_args(wchar_t *filename, int argc, wchar_t **argv)
+void process_file_args(char *filename, int argc, char **argv)
+{
+    wchar_t **changed_argv = convert_argv_char_to_wchar(argc, argv);
+
+    if (changed_argv)
+    {
+        process_file_wargs(filename, argc, changed_argv);
+
+        // Cleanup
+        for (int i = 0; i < argc; i++)
+        {
+            if (changed_argv[i])
+            {
+                fprintf(stderr, "Freeing %p\n", changed_argv[i]);  // DEBUGGING
+                free(changed_argv[i]);
+                changed_argv[i] = NULL;
+            }
+        }
+        fprintf(stderr, "Freeing %p\n", changed_argv);  // DEBUGGING
+        free(changed_argv);
+        changed_argv = NULL;
+    }
+}
+
+
+// int wmain(int argc, wchar_t **argv)
+int main(int argc, char **argv)
 {
     Py_Initialize();
 
     /* MY CODE */
     if (argc != 2)
     {
+        // fprintf(stderr, "USAGE: %ls FILENAME.py\n", argv[0]);
         fprintf(stderr, "USAGE: %s FILENAME.py\n", argv[0]);
         return 1;
     }
     else
     {
-        process_file(argv[1]);
+        // process_file(argv[1]);
+        process_file_args(argv[1], argc, argv);
     }
 
     Py_Finalize();
