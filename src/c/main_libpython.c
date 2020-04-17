cb8acbf9b6072bf02fa01de85b53d73556401be8
hark130 <hark130@yahoo.com>
2020-04-17 12:23:03 -0500

    development: Cleaning up main_libpython.c
    Still!
    
    Changes to be committed:
    	modified:   src/c/main_libpython.c

--- src/c/main_libpython.c
+++ src/c/main_libpython.c
@@ -142,10 +142,7 @@ int main(int argc, char **argv)
     // LOCAL VARIABLES
     int status = EXIT_SUCCESS;    // Exit status
     int err_num = 0;              // Store errno here
-    // char *cmdBuff = NULL;         // Buffer to store the command
-    // FILE *python_stream = NULL;   // Return value from the call to popen()
-    // int temp_char = 0;            // Store characters from the process stream
-    wchar_t **wargv = NULL;
+    wchar_t **wargv = NULL;       // Converted argv to wchar_t
 
 
     // INPUT VALIDATION
